890eef3b326fdd6e7982a78aeb7904619da5bcb7
Deepak Sah <deepak.sah@nutanix.com>
2023-06-17 00:30:54 +0530

    3b: multi-node broadcast

--- single-node-broadcast/main.go
+++ single-node-broadcast/main.go
@@ -4,17 +4,25 @@ import (
 	"encoding/json"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 	"log"
+	"sync"
 )
 
 type server struct {
-	node *maelstrom.Node
-	neighbourNodes []string
-	msgIDReceived []float64
+	node             *maelstrom.Node
+	neighbourNodes   []string
+	msgIDReceived    []float64
+	msgIDMutex       *sync.RWMutex
+	msgReceiveCh     chan msgBroadcast
+	receivedMessages map[float64]struct{}
+}
+
+type msgBroadcast struct {
+	src  string
+	body map[string]any
 }
 
 func newServer(node *maelstrom.Node) *server {
-	node.NodeIDs()
-	return &server{node: node, msgIDReceived: []float64{}}
+	return &server{node: node, msgIDReceived: []float64{}, msgIDMutex: &sync.RWMutex{}, msgReceiveCh: make(chan msgBroadcast), receivedMessages: map[float64]struct{}{}}
 }
 
 type messageType string
@@ -25,15 +33,29 @@ func (mType messageType) toString() string {
 
 const (
 	BROADCAST messageType = "broadcast"
-	READ messageType = "read"
-	TOPOLOGY messageType = "topology"
+	READ      messageType = "read"
+	TOPOLOGY  messageType = "topology"
+	INIT      messageType = "init"
 )
 
-func (s *server) initHandler(msg maelstrom.Message) error {
+func (s *server) broadcastToNodes(src string, body map[string]any) error {
 	for _, nodeID := range s.node.NodeIDs() {
 		s.neighbourNodes = append(s.neighbourNodes, nodeID)
 	}
-	return s.node.Reply(msg, nil)
+	return s.broadcast(src, body)
+}
+
+func (s *server) broadcast(src string, body map[string]any) error {
+	for _, destNodeID := range s.neighbourNodes {
+		if s.node.ID() != destNodeID && src != destNodeID {
+			go func() {
+				if err := s.node.Send(destNodeID, body); err != nil {
+					log.Fatal(err)
+				}
+			}()
+		}
+	}
+	return nil
 }
 
 func (s *server) broadcastHandler(msg maelstrom.Message) error {
@@ -42,24 +64,33 @@ func (s *server) broadcastHandler(msg maelstrom.Message) error {
 		return err
 	}
 
-	s.msgIDReceived = append(s.msgIDReceived, body["message"].(float64))
+	msgValue := body["message"].(float64)
+	// Store only if it's not already present.
+	s.msgIDMutex.Lock()
+	if _, ok := s.receivedMessages[msgValue]; ok {
+		s.msgIDMutex.Unlock()
+		return nil
+	}
+	s.receivedMessages[msgValue] = struct{}{}
+	s.msgIDMutex.Unlock()
+	s.broadcastToNodes(msg.Src, body)
 
 	reply := map[string]any{
 		"type": "broadcast_ok",
 	}
-	s.node.NodeIDs()
 	return s.node.Reply(msg, reply)
 }
 
 func (s *server) readHandler(msg maelstrom.Message) error {
-	var body map[string]any
-	if err := json.Unmarshal(msg.Body, &body); err != nil {
-		return err
+	var replyMessages []float64
+	s.msgIDMutex.RLock()
+	for msg := range s.receivedMessages {
+		replyMessages = append(replyMessages, msg)
 	}
-
+	s.msgIDMutex.RUnlock()
 	reply := map[string]any{
-		"type": "read_ok",
-		"messages": s.msgIDReceived,
+		"type":     "read_ok",
+		"messages": replyMessages,
 	}
 	return s.node.Reply(msg, reply)
 }
